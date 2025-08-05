9b8490eaea29bc7ef6abf652aa2ee0f6fb1ff6d1
c4chakri <chakravarthi.l@mobiusdtaas.ai>
2025-08-05 11:56:14 +0530

    TI-1173 Develop Mock apis for pi-entitiy-schema for unrestricted location

--- index.js
+++ index.js
@@ -8,8 +8,33 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Proxy route
+app.post('/tokens', async (req, res) => {
+    try {
+        console.log("Getting tokens data..... from proxy");
+        
+        const apiUrl = 'https://ig.gov-cloud.ai/pi-entity-instances-service/v2.0/schemas/685e7a342ec4242da906daca/instances/list?page=0&size=50&showDBaaSReservedKeywords=true&showPageableMetaData=true';
+
+        const response = await axios.post(apiUrl, req.body, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.BEARER_TOKEN}`
+            }
+        });
+
+        res.status(200).json(response.data);
+    } catch (error) {
+        console.error('Proxy Error:', error.message);
+        res.status(error.response?.status || 500).json({
+            error: error.message,
+            details: error.response?.data || {}
+        });
+    }
+});
+
 app.post('/proxy', async (req, res) => {
     try {
+        console.log("Getting proxy data..... from proxy");
+        
         const apiUrl = 'https://ig.gov-cloud.ai/pi-entity-instances-service/v2.0/schemas/68661aea2ec4242da906ee0f/instances/list?page=0&size=50&showDBaaSReservedKeywords=true&showPageableMetaData=true';
 
         const response = await axios.post(apiUrl, req.body, {
@@ -28,6 +53,35 @@ app.post('/proxy', async (req, res) => {
         });
     }
 });
+
+app.post('/investors', async (req, res) => {
+    try {
+        console.log("Getting investors data..... from proxy");
+        
+        const apiUrl = 'https://ig.gov-cloud.ai/pi-entity-instances-service/v2.0/schemas/68661aea2ec4242da906ee0f/instances/list?page=0&size=50&showDBaaSReservedKeywords=true&showPageableMetaData=true';
+
+        const response = await axios.post(apiUrl, req.body, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.BEARER_TOKEN}`
+            }
+        });
+
+        res.status(200).json(response.data);
+    } catch (error) {
+        console.error('Proxy Error:', error.message);
+        res.status(error.response?.status || 500).json({
+            error: error.message,
+            details: error.response?.data || {}
+        });
+    }
+});
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 })
